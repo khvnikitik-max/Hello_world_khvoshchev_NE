56c5ff5bfe44fff2e72f3a2346532045fae81211
khvnikitik-max <khv.nikitik@gmail.com>
2026-02-19 14:19:58 +0300

    Add files via upload

--- projects_2/task_2_3/task_2-3_lab_journal.py
+++ projects_2/task_2_3/task_2-3_lab_journal.py
@@ -6,7 +6,7 @@ fio_processed = fio.strip().upper();
 date_processed = date.strip().upper();
 name_processed = name.strip().upper();
 conclusion_processed = conclusion.strip().upper();
-text = (f"")
+text = (f"______________________________________________\nЭлектронный лабораторный журнал\n______________________________________________\nФИО исследователя:\t{fio_processed}\nДата:\t{date_processed}\nЭксперимент:\t{name_processed}\nВывод:\t{conclusion_processed}\n______________________________________________")
 
 # Запись данных в текстовой файл
 
